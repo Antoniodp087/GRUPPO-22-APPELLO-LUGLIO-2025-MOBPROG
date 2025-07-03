5e970831c521bb11f02f9930502ef56fc2fc8bf8
Antonio <124215080+Antoniodp087@users.noreply.github.com>
2025-07-03 17:03:15 +0200

    try on checkboxListTile

--- plant_care_app/lib/main.dart
+++ plant_care_app/lib/main.dart
@@ -9,10 +9,21 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return SafeArea(
+      child: MaterialApp(
+        debugShowMaterialGrid: false,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        ),
+        home: Scaffold(
+          body: Center(
+            child: CheckboxListTile(
+              title: const Text('CheckboxListTile with red background'),
+              value: false,
+              onChanged: (value) => (),
+            ),
+          ),
         ),
       ),
     );
