87ee94dd5f3f35f5c3d433a6b54d67e5efde324a
Antonio <124215080+Antoniodp087@users.noreply.github.com>
2025-07-03 18:17:36 +0200

    Adding Style and GoogleFont

---
+++ plant_care_app/lib/styles/app_style.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppStyle {
+  static Color iconActivated = Color(0xFF668964);
+  static Color iconFilled = Color(0xFF668964);
+  static Color iconUnactivated = Color(0xFF757573);
+
+  static TextStyle headLine1 = GoogleFonts.quicksand(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+  );
+}
